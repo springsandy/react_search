67f4c110fac0d29a14491f5d736864f3db5409c1
김봄이 <s2018w26@e-mirim.hs.kr>
2023-06-08 01:16:37 +0900

    modify search

--- src/components/layout/Search/search.js
+++ src/components/layout/Search/search.js
@@ -1,12 +1,20 @@
+import React, {useState} from 'react';
 import './search.css';
 
-const search = () => {
+const Search = (event) => {
+  const [searchText, setSearchText] = useState()
+  
+  const search_button = () => {
+    console.log('aaa')
+    console.log(searchText)
+  }
+
   return (
-    <div class="search">
-      <input type="text"  placeholder="검색어 입력"/>
-      <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"/>
+    <div className="search">
+      <input type="text" value={searchText} placeholder="검색어 입력" />
+      <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" onClick={search_button} />
     </div>
   );
 }
 
-export default search;
+export default Search;
