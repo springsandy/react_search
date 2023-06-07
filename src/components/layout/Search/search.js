68a0b4260ca525197561e1307e5c970c3295ab76
김봄이 <s2018w26@e-mirim.hs.kr>
2023-06-08 00:09:59 +0900

    add search

---
+++ src/components/layout/Search/search.js
@@ -0,0 +1,12 @@
+import './search.css';
+
+const search = () => {
+  return (
+    <div class="search">
+      <input type="text"  placeholder="검색어 입력"/>
+      <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"/>
+    </div>
+  );
+}
+
+export default search;
